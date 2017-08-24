24aac9bb0e7b674c397a51fa59bca5ff8f298283
Allan James Lapid <ajLapid718@gmail.com>
2017-08-24 02:34:15 -0400

    Rails db:migrate

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,8 +8,11 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :directions,
                                 reject_if: :all_blank,
                                 allow_destroy: true
-
+                                
   validates :title, :description, :image, presence: true
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  belongs_to :user
+
 end
